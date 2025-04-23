827bc506682188559d88b35cd53950b5588085bf
João Vitor <joaovtrmorais0@gmail.com>
2025-04-23 12:01:45 -0300

    feat: Implementing registry_order_validator

---
+++ src/validators/registry_order_validator_test.py
@@ -0,0 +1,34 @@
+import pytest
+from .registry_order_validator import registry_order_validator
+
+
+def test_registry_order_validator():
+    body = {
+        "data": {
+            "name": "joaozinho",
+            "address": "rua do limao",
+            "cupom": False,
+            "items": [
+                {"item": "refrigerante", "quantidade": 2},
+                {"item": "pizza", "quantidade": 3}
+            ]
+        }
+    }
+    registry_order_validator(body)
+
+
+def test_registry_order_validator_with_errors():
+    body_with_error = {
+        "data": {
+            "name": "joaozinho",
+            "address": "rua do limao",
+            "cupom": "error",
+            "items": [
+                {"item": "refrigerante", "quantidade": 2},
+                {"item": "pizza", "quantidade": 3}
+            ]
+        }
+    }
+
+    with pytest.raises(Exception):
+        registry_order_validator(body_with_error)
